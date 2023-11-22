8165fe896683533bf997b1dfbe8c8d1876f4857d
InGyu Moon <ansdlsrb1121@naver.com>
2023-11-23 00:00:41 +0900

    feat: MemberEntity, MeetingEntity 구현

---
+++ src/main/java/group/meetmix/data/entity/MeetingEntity.java
@@ -0,0 +1,51 @@
+package group.meetmix.data.entity;
+
+import group.meetmix.data.dto.MeetingDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "MEETING")
+@Getter
+@Setter
+public class MeetingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEETING_ID")
+    private Long meetingId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private MemberEntity member;
+    @Column(name = "TITLE")
+    private String title;
+    @Column(name = "CONTENT")
+    private String content;
+    @Column(name = "CREATE_AT")
+    private LocalDateTime createAt;
+    @Column(name = "DEADLINE")
+    private LocalDateTime deadline;
+    @Column(name="MAX_CAPACITY")
+    private int maxCapacity;
+
+    // == 연관 관계 메서드 == //
+    public void setMemberEntity(MemberEntity member){
+        this.member = member;
+        member.getMeetings().add(this);
+    }
+
+    // == 생성 메서드 == //
+    public static MeetingEntity createMeeting(MemberEntity member, MeetingDto meetingDto){
+        MeetingEntity meetingEntity = new MeetingEntity();
+        meetingEntity.setMemberEntity(member);
+        meetingEntity.setTitle(meetingDto.getTitle());
+        meetingEntity.setContent(meetingDto.getContent());
+        meetingEntity.setCreateAt(LocalDateTime.now());
+        //meetingEntity.setDeadline(meetingDto.getDeadline());
+        meetingEntity.setMaxCapacity(meetingDto.getMaxCapacity());
+        return  meetingEntity;
+    }
+
+}
--- src/main/java/group/meetmix/data/entity/MemberEntity.java
+++ src/main/java/group/meetmix/data/entity/MemberEntity.java
@@ -4,17 +4,26 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
-@Table(name = "member")
+@Table(name = "MEMBER")
 @Getter
 @Setter
 public class MemberEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
     private Long memberId;
-    @Column(unique = true)
+
+    @Column(unique = true,name = "MEMBER_EMAIL")
     private String memberEmail;
+    @Column(name = "PASSWORD")
     private String password;
-    @Column(unique = true)
+    @Column(unique = true,name = "NICKNAME")
     private String nickname;
+
+    @OneToMany(mappedBy = "member")
+    private List<MeetingEntity> meetings = new ArrayList<>();
 }
