f9daaf9aeec55bfe320b78325ffe22dd69ca6f5f
InGyu Moon <ansdlsrb1121@naver.com>
2023-11-24 02:37:43 +0900

    feat: ApplyEntity 구현

--- src/main/java/group/meetmix/data/entity/ApplyEntity.java
+++ src/main/java/group/meetmix/data/entity/ApplyEntity.java
@@ -1,5 +1,6 @@
 package group.meetmix.data.entity;
 
+import group.meetmix.data.repository.ApplyRepository;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,5 +23,22 @@ public class ApplyEntity {
     @JoinColumn(name = "MEETING_ID")
     private MeetingEntity meeting;
 
+    // == 연관 관계 메서드 == //
+    public void setMemberEntity(MemberEntity member){
+        this.member = member;
+        member.getApplies().add(this);
+    }
+    // == 연관 관계 메서드 == //
+    public void setMeetingEntity(MeetingEntity meeting){
+        this.meeting = meeting;
+        meeting.getApplies().add(this);
+    }
+    // == 생성 메서드 == //
+    public static ApplyEntity createApply(MemberEntity member, MeetingEntity meeting){
+        ApplyEntity applyEntity = new ApplyEntity();
+        applyEntity.setMember(member);
+        applyEntity.setMeeting(meeting);
+        return applyEntity;
+    }
 
 }
